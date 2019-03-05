bf0d94520d0430fca82803550e641f590c780e8b
AlaaAlhilly <alaa.naji.us@gmail.com>
2019-03-04 23:06:33 -0600

    Updates

---
+++ precache-manifest.3d0b5a9265b408cc2517667faec7ab98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "78ea12051b1d780dca99",
+    "url": "/clicky-final-game/static/css/main.a25f0685.chunk.css"
+  },
+  {
+    "revision": "78ea12051b1d780dca99",
+    "url": "/clicky-final-game/static/js/main.78ea1205.chunk.js"
+  },
+  {
+    "revision": "2393c616efdd99eb549d",
+    "url": "/clicky-final-game/static/js/runtime~main.2393c616.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/clicky-final-game/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "ac1cd9ce65b38c9138c6f0c826953890",
+    "url": "/clicky-final-game/index.html"
+  }
+];
